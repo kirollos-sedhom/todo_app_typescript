b713515835344636927b3613fde77b104b9781fc
carlo <kirollosedhom@gmail.com>
2025-02-16 11:09:49 +0200

    fix input not blurring

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,49 @@
+import React, { useRef } from "react";
 import "./App.css";
 import InputField from "./components/InputField";
+import { Todo } from "./model";
 function App() {
+  const [todoItem, setTodoItem] = React.useState<string>("");
+  const [todoList, setTodoList] = React.useState<Todo[]>([]);
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  function handleAdd(e: React.FormEvent) {
+    e.preventDefault();
+    let newTodoItem = {
+      id: Date.now().toString(),
+      task: todoItem,
+      isDone: false,
+    };
+    if (todoItem) {
+      setTodoList((prev) => [newTodoItem, ...prev]);
+      setTodoItem("");
+
+      inputRef.current?.blur();
+    }
+  }
+  console.log(todoList);
   return (
-    <div className="bg-blue-400 h-screen m-0 p-p">
-      <h1 className="text-center py-4 text-2xl text-white z-1">Taskify</h1>
-      <InputField />
+    <div className="bg-blue-700 h-screen m-0 p-p">
+      <h1 className="text-center py-4 text-3xl text-white z-10 relative">
+        Taskify
+      </h1>
+      <InputField
+        todoItem={todoItem}
+        setTodoItem={setTodoItem}
+        todoList={todoList}
+        setTodoList={setTodoList}
+        handleAdd={handleAdd}
+        inputRef={inputRef}
+      />
+      <div className="w-9/10 m-auto bg-blue-400 rounded-sm p-2 my-2">
+        <h2 className="text-white text-xl">Active Tasks</h2>
+        <ul>
+          {todoList.map((item) => (
+            <li className="bg-linear-to-r from-yellow-100 to-white  my-2 p-2 rounded-md">
+              {item.task}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,18 +1,40 @@
-import React from "react";
+import React, { useRef } from "react";
+import { Todo } from "../model";
 
-const InputField = () => {
+type Props = {
+  todoItem: string;
+  setTodoItem: React.Dispatch<React.SetStateAction<string>>;
+  todoList: Todo[];
+  setTodoList: React.Dispatch<React.SetStateAction<Todo[]>>;
+  handleAdd: (e: React.FormEvent) => void;
+  inputRef: React.MutableRefObject<HTMLInputElement | null>;
+};
+const InputField = ({
+  todoItem,
+  setTodoItem,
+  todoList,
+  setTodoList,
+  handleAdd,
+  inputRef,
+}: Props) => {
   return (
-    <form className="w-9/10 mx-auto relative flex justify-center items-center">
+    <form
+      onSubmit={(e) => handleAdd(e)}
+      className="w-9/10 mx-auto relative flex justify-center items-center"
+    >
       <input
-        className="w-full bg-white rounded-4xl p-2"
+        ref={inputRef}
+        value={todoItem}
+        onChange={(e) => setTodoItem(e.target.value)}
+        className="w-full bg-white rounded-4xl p-2 placeholder-gray-900 focus:shadow-[0_0_10px_1000px_rgba(0,0,0,0.5)] "
         type="input"
         placeholder="what would you like to do?"
       />
       <button
         type="submit"
-        className="absolute right-2 bg-blue-400 text-white rounded-4xl p-1 cursor-pointer"
+        className="absolute right-2 bg-blue-700 text-white rounded-4xl p-1 cursor-pointer shadow-[0_0_10px_black] "
       >
-        add
+        GO
       </button>
     </form>
   );
